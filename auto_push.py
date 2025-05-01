02ae162cd57dabd3ad7657548d07f4838d3468bb
Zeyu Li <zeyuli0123@icloud.com>
2025-05-02 05:58:10 +0800

    upload pred

---
+++ auto_push.py
@@ -0,0 +1,16 @@
+import subprocess
+import time
+
+def run_git_commands():
+    try:
+        subprocess.run(["git", "add", "."], check=True)
+        subprocess.run(["git", "commit", "-m", "upload longbench log"], check=True)
+        subprocess.run(["git", "push"], check=True)
+        print("Git push successful.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+
+if __name__ == "__main__":
+    while True:
+        run_git_commands()
+        time.sleep(10)
