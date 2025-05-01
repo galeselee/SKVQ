8794538e7803f5030b7d7b993fe1b9793858d5c1
Zeyu Li <zeyuli0123@icloud.com>
2025-05-02 05:59:15 +0800

    auto upload longbench log

--- auto_push.py
+++ auto_push.py
@@ -4,7 +4,7 @@ import time
 def run_git_commands():
     try:
         subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(["git", "commit", "-m", "upload longbench log"], check=True)
+        subprocess.run(["git", "commit", "-m", "auto upload longbench log"], check=True)
         subprocess.run(["git", "push"], check=True)
         print("Git push successful.")
     except subprocess.CalledProcessError as e:
